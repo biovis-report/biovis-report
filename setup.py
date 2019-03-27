5a456475ab7aaa07d2d7e3e0614fcb861c25b370
YJC <yjcyxky@163.com>
2019-03-27 22:15:57 +0800

    增加conda recipe.

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
         'PyYAML>=3.10',
         'tornado>=5.0'
     ],
-    python_requires='>=2.7.9,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+    python_requires='!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
     entry_points={
         'console_scripts': [
             'mkdocs = mkdocs.__main__:cli',
