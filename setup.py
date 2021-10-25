de0cb64dc1b59c25c24b0d784298cd05299b411a
YJC <yjcyxky@163.com>
2021-10-25 14:11:22 +0800

    Improve dependencies.

--- setup.py
+++ setup.py
@@ -68,7 +68,8 @@ setup(
         "coloredlogs",
         "argcomplete",
         "pymdown-extensions",
-        "verboselogs>=1.7"
+        "verboselogs>=1.7",
+        "jsonschema>=4.1.2"
     ],
     python_requires="!=2.7.*,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*",
     entry_points={
